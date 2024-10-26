718de461a6b15aa2bcf9b9a9c657cee4e72faa0f
nynforeva <watahelnya987@gmail.com>
2024-10-26 10:26:49 +0500

    yep

--- my-docker-app/app.py
+++ my-docker-app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
  return "Hello, Docker with Flask!"
 
-if name == "main":
+if __name__ == "__main__":
  app.run(host="0.0.0.0", port = 5000)
